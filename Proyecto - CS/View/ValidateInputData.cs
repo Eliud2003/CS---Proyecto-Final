7e0b8d887dacf8ae91176185976551f9f14087f4
ELIUD ALFONSO SUAREZ CLEMENTE <eliud.suarezcle@ug.edu.ec>
2024-07-29 18:24:17 -0500

    class validate

---
+++ Proyecto - CS/View/ValidateInputData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Proyecto___CS.View
+{
+    internal class ValidateInputData
+    {
+        internal static bool OnlyNumber(KeyPressEventArgs e, TextBox txtnum)
+        {
+            if (char.IsNumber(e.KeyChar) && txtnum.Text.Length < 3)
+            {
+                e.Handled = false;
+                return true;
+            }
+            else if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+                return true;
+            }
+            else
+            {
+                e.Handled = true;
+                return false;
+            }
+        }
+        public static bool Onlyletters(KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+                return true;
+            }
+            else if (char.IsControl(e.KeyChar) || e.KeyChar == ' ')
+            {
+                e.Handled = false;
+                return true;
+            }
+            else
+            {
+                e.Handled = true;
+                return false;
+            }
+        }
+        public static bool txtEmpty(TextBox txt)
+        {
+            if (txt.Text == string.Empty)
+            {
+                txt.Focus();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
